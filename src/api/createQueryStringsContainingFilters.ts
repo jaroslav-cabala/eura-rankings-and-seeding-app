75fa5058ac60c71b22f39619bc3cdbbfa93767c5
jaroslav-cabala <cab.jaro@gmail.com>
2024-09-20 23:55:52 +0200

    updating function that creates a queryString with filters to allow filtering by  multiple divisions

--- src/api/createQueryStringsContainingFilters.ts
+++ src/api/createQueryStringsContainingFilters.ts
@@ -1,8 +1,10 @@
 import { Division } from "@/domain";
 import { TimePeriod } from "@/utils";
 
-export const createQueryString = (division?: Division, seasons?: TimePeriod): string => {
-  const divisionParam = division ? `division=${division}` : "";
+export const createQueryString = (division?: Array<Division>, seasons?: TimePeriod): string => {
+  const divisionParam = division
+    ? division.map((division, index) => `${index ? "&" : ""}division=${division}`)
+    : "";
   const seasonsParam = seasons
     ? `seasons${encodeURI(`[from]=${seasons.from}`)}&seasons${encodeURI(`[to]=${seasons.to}`)}`
     : "";
